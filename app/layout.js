1cfbf57081d480e22a2a88be301fb4a3fb670ac4
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-02 15:25:21 -0300

    Adds ClerkProvider to the root layout for authentication management

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "500", "600", "700", "800", "900"],
@@ -13,8 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
