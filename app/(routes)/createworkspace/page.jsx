29cb1abd6c637ac20501750494075b750dd7595e
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-05 17:34:41 -0300

    implement basic create workspace form with name and emoji input fields

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -1,41 +1,56 @@
 "use client";
+import CoverPicker from "@/app/_components/CoverPicker";
+import EmojiPickerComponent from "@/app/_components/EmojiPickerComponent";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { SmilePlus } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 
 function CreateWorkspace() {
   const [coverImage, setCoverImage] = useState("/cover.png");
+  const [workspaceName, setWorkspaceName] = useState();
+  const [emoji, setEmoji] = useState();
   return (
     <div className="p-10 md:px-36 lg:px-52 xl:px-80 py-20">
-      <div>
+      <div className="shadow-2xl rounded-xl">
         {/* Cover Image */}
-        <div className="relative group cursor-pointer">
-          <h2 className=" hidden group-hover:flex absolute p-4 w-full h-full items-center justify-center flex">
-            Change Cover
-          </h2>
-          <div className="group-hover:opacity-65">
-            <Image
-              src={coverImage}
-              width={400}
-              height={400}
-              alt="cover-img"
-              className="w-full h-[150px] object-cover rounded-t-xl"
-            />
+        <CoverPicker setNewCover={(v) => setCoverImage(v)}>
+          <div className="relative group cursor-pointer">
+            <h2 className=" hidden group-hover:flex absolute p-4 w-full h-full items-center justify-center flex">
+              Change Cover
+            </h2>
+            <div className="group-hover:opacity-65">
+              <Image
+                src={coverImage}
+                width={400}
+                height={400}
+                alt="cover-img"
+                className="w-full h-[200px] object-cover rounded-t-xl"
+              />
+            </div>
           </div>
-        </div>
+        </CoverPicker>
 
         {/* Input section */}
         <div className="p-12">
           <h2 className="font-bold text-xl">Create a new workspace</h2>
           <h2 className="text-sm mt-2">
             This is a shared space where you can collaborate with your team. You
-            can always rename it later
+            can always rename it later.
           </h2>
-          <div className="mt-8">
-            <Button variant="outline">
-              <SmilePlus />
-            </Button>
+          <div className="mt-8 flex gap-3 items-center">
+            <EmojiPickerComponent setEmojiIcon={(v) => setEmoji(v)}>
+              <Button variant="outline">{emoji ? emoji : <SmilePlus />}</Button>
+            </EmojiPickerComponent>
+            <Input
+              placeholder="Workspace Name"
+              onChange={(e) => setWorkspaceName(e.target.value)}
+            />
+          </div>
+          <div className="mt-7 flex justify-end gap-6">
+            <Button disabled={!workspaceName?.length}>Create</Button>
+            <Button variant="outline">Cancel</Button>
           </div>
         </div>
       </div>
