a9774020254070b45f80d6dec757d307a6e211b9
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-05 17:29:35 -0300

    added default cover images for selection

---
+++ app/_shared/CoverOption.js
@@ -0,0 +1,39 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowArray": true}] */
+export default [
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/geometric-seamless-pattern_23-2151021459.jpg?ga=GA1.1.1679917421.1705195646&semt=sph",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/vibrant-colors-flow-abstract-wave-pattern-generated-by-ai_188544-9781.jpg?ga=GA1.1.1679917421.1705195646&semt=sph",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/abstract-psychedelic-papercut-background_23-2149303027.jpg?ga=GA1.1.1679917421.1705195646&semt=sph",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-vector/geometric-wallpaper-japanese-style_52683-34401.jpg?ga=GA1.1.1679917421.1705195646&semt=sph",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/photorealistic-galaxy-background_23-2151064385.jpg?ga=GA1.1.1679917421.1705195646&semt=ais_user_ai_gen",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/glowing-spaceship-orbits-sphere-deep-space-generated-by-ai_188544-9658.jpg?ga=GA1.1.1679917421.1705195646&semt=ais_user_ai_gen",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/colorful-splash-paint-white-background_1409-5118.jpg?ga=GA1.1.1679917421.1705195646&semt=ais_user_ai_gen",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/minimalist-colorful-3d-lines-background_1409-4965.jpg?t=st=1721822568~exp=1721826168~hmac=63610a79e449250105959bd41a3af2f9018622345d71625821a1d1affaf510fa&w=2000",
+  },
+  {
+    imageUrl:
+      "https://img.freepik.com/free-photo/seamless-abstract-pattern-design_23-2151179146.jpg?ga=GA1.1.1679917421.1705195646&semt=ais_user_ai_gen",
+  },
+];
