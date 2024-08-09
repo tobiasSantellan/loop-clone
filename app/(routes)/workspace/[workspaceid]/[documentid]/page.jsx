38c3b658402932141f5e0e64414886aff1ae0ac7
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-08-08 22:21:33 -0300

    add workspace document layout with side nav

--- app/(routes)/workspace/[workspaceid]/[documentid]/page.jsx
+++ app/(routes)/workspace/[workspaceid]/[documentid]/page.jsx
@@ -1,7 +1,20 @@
+"use client";
 import React from "react";
+import SideNav from "../../_components/SideNav";
 
-function WorkspaceDocument() {
-  return <div>WorkspaceDocument</div>;
+function WorkspaceDocument({ params }) {
+  return (
+    <div>
+      {/* Side Nav */}
+      <div>
+        <SideNav params={params} />
+      </div>
+
+      {/* Document */}
+
+      <div className="md:ml-72">Document</div>
+    </div>
+  );
 }
 
 export default WorkspaceDocument;
